d8baccbeb6ab79220a6ec21ab6033c47d553d530
jhtkoo0426 <jhtkoo@gmail.com>
2022-10-31 22:15:30 +0000

    Added simple interface for admin

--- guiana/accounts/admin.py
+++ guiana/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "phone", "first_name")
+    list_filter = ("username", )
