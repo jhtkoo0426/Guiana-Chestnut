a09498798f3eea4a546f3f5408226b8534ff7445
jhtkoo0426 <jhtkoo@gmail.com>
2022-10-31 22:15:23 +0000

    Updated custom user model and settings

--- guiana/accounts/models.py
+++ guiana/accounts/models.py
@@ -1,67 +1,102 @@
-from django.contrib.auth.models import (AbstractUser, BaseUserManager,
-                                        PermissionsMixin)
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_superuser(self, username, email, first_name, password=None, is_admin=True):
+    def create_user(self, email, username, phone=None, password=None, is_active=True, is_staff=False,
+                    is_admin=False):
         """
-        Creates and saves a superuser instance with the given username and password.
+        Creates and saves a User with the given email and password.
         """
+        if not email:
+            raise ValueError('Users must have an email address')
 
-        user = self.create_user(
-            username=username,
-            email=email,
-            first_name=first_name,
-            password=password,
-        )
-
-        user.is_admin = is_admin
-
-        return user
-    
-    def create_user(self, username, email, first_name, password=None):
-        """
-        Creates and saves a basic user isntance with the given username and password.
-        """
+        if not password:
+            raise ValueError('Users must have a password')
 
         if not username:
-            raise ValueError("Users must have a username!")
-    
-        if not email:
-            raise ValueError("Users must have an email address!")
-        
-        if not password:
-            raise ValueError("Users must have a password!")
-        
-        email = self.normalize_email(email)
+            raise ValueError('Users must have a username')
+
         user = self.model(
-            email=email,
+            email=self.normalize_email(email),
             username=username,
-            first_name=first_name
+            phone=phone,
         )
 
         user.set_password(password)
+        user.staff = is_staff
+        user.admin = is_admin
+        user.active = is_active
         user.save(using=self._db)
         return user
-    
 
-class CustomUser(AbstractUser, PermissionsMixin):
-    """
-    An abstract base class implementing a User model.
-    """
+    def create_staffuser(self, email, password, username):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            username,
+            password=password,
+            is_staff=True,
+        )
 
-    username = models.CharField(max_length=50, blank=False, null=True, unique=True)
-    email = models.EmailField(blank=False, null=True, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
+        return user
+
+    def create_superuser(self, email, password, username, phone):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            username,
+            password=password,
+            phone=phone,
+            is_staff=True,
+            is_admin=True,
+        )
+
+        return user
+
+
+class CustomUser(AbstractUser):
+    # User property fields
+    username = models.CharField(max_length=50, blank=True, null=True, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=50, blank=True, null=True, unique=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True, unique=False)
     finnhub_api_key = models.CharField(max_length=100, blank=True, null=True, unique=True)
 
-    is_admin = models.BooleanField(default=False)
+    # User type fields
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'first_name']
+    REQUIRED_FIELDS = ['email', 'phone']
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    # Inherited functions from AbstractBaseUser
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        # Is the user a member of staff?
+        return self.staff
+
+    @property
+    def is_admin(self):
+        # Is the user a admin member?
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
