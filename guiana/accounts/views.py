6c2bb03ccf1c6f40b28abdcfaa1a54a88cb2bb2c
jhtkoo0426 <jhtkoo@gmail.com>
2022-11-01 21:24:58 +0000

    Added views and urls for authentication

--- guiana/accounts/views.py
+++ guiana/accounts/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.views.generic import View
 
-# Create your views here.
+from .forms import UserLoginForm, UserRegisterForm
+
+
+class LoginPageView(View):
+    template_name = "authentication/login.html"
+    
+    def get(self, request):
+        login_form = UserLoginForm()
+        message = ""
+
+        context = {
+            "login_form": login_form,
+            "message": message
+        }
+        return render(request, self.template_name, context=context)
+    
+    def post(self, request):
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = authenticate(
+                username=login_form.cleaned_data["username"],
+                password=login_form.cleaned_data["password"],
+            )
+
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+        
+        message = "Login failed. Please enter the correct username or password."
+        
+        context = {
+            "login_form": login_form,
+            "message": message
+        }
+        return render(request, self.template_name, context=context)
+
+
+class RegisterPageView(View):
+    template_name = "authentication/register.html"
+
+    def get(self, request):
+        register_form = UserRegisterForm()
+        message = ""
+
+        context = {
+            "register_form": register_form,
+            "message": message
+        }
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        register_form = UserRegisterForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+            return redirect('login')
+        else:
+            context = {
+                "register_form": register_form
+            }
+            return render(request, self.template_name, context=context)
+        
+
+class LogoutPageView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
--- guiana/guiana/urls.py
+++ guiana/guiana/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from accounts.views import LoginPageView, LogoutPageView, RegisterPageView
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterPageView.as_view(), name='register'),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('logout/', LogoutPageView.as_view(), name='logout'),   
 ]
