f663b0f420bd148148e62ddd37b8f16b0d88ef66
Justin Koo <jhtkoo@gmail.com>
2022-11-04 11:58:51 +0000

    Updated finnhub app name to solve conflicts

--- guiana/finnhub_integration/apps.py
+++ guiana/finnhub_integration/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FinnhubConfig(AppConfig):
+class FinnhubIntegrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'finnhub'
+    name = 'finnhub_integration'
---
+++ guiana/finnhub_integration/finnhub_api.py
@@ -0,0 +1,5 @@
+"""
+Integrates finnhub.io methods into Guiana for stock symbol data processing, analysing and visualisation.
+"""
+
+import finnhub as fh
