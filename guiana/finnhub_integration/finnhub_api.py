6a80f3d5bce7e551100b9f32cb20b1a219c6758a
jhtkoo0426 <jhtkoo@gmail.com>
2022-11-05 15:31:09 +0000

    Added function and file for extracting all finnhub supported exchanges

--- guiana/finnhub_integration/finnhub_api.py
+++ guiana/finnhub_integration/finnhub_api.py
@@ -3,3 +3,80 @@ Integrates finnhub.io methods into Guiana for stock symbol data processing, anal
 """
 
 import finnhub as fh
+import requests
+import csv
+
+from finnhub_integration.models import FinnhubSupportedExchanges
+
+
+class FinnhubClient:
+    def __init__(self):
+        """
+        Construct a new finnhub.io Client instance.
+
+        @return: returns nothing
+        """
+
+        self.api_key = None
+        self.client = None
+        self.API_URL = "https://finnhub.io/api/v1/?token="
+    
+    def update_key(self, key: str):
+        """
+        Updates the current api_key with a new key; If no key was previously set, 
+        use the key as the current key.
+
+        @param key: a registered API key from finnhub.io
+        @return: returns nothing
+        """
+
+        if self.check_key_valid(key):
+            self.api_key = key
+        else:
+            raise ValueError("The API key you entered is invalid or has expired. Pleas try again.")
+    
+    def check_key_valid(self, key):
+        """
+        Sends a request to finnhub.io to validate the provided key.
+        @return: Returns True if the key is valid, False otherwise.
+        """
+        
+        url = self.API_URL + key
+        
+        # Check if exception is raised for any endpoint
+        r = requests.get(url)
+        return r.status_code != 401
+
+    def initialize_client(self):
+        """
+        Establish a connection to the Finnhub API using the current api key.
+
+        @return: returns nothing
+        """
+
+        self.client = fh.Client(api_key=self.api_key)
+
+    # TODO: Convert this function to scheduled function (via celery)
+    def import_supported_exchanges(self):
+        """
+        Import a list of supported exchanges from finnhub.io.
+
+        @return: returns nothing
+        """
+
+        with open('../Finnhub Exchanges.csv', newline='') as f:
+            reader = csv.reader(f)
+            next(reader)    # To skip the labels row
+            
+            for row in reader:
+                # ['code', 'name', 'mic', 'timezone', 'hour', 'close_date', 'country', 'source', '']
+                new_exchange = FinnhubSupportedExchanges.objects.create(
+                    exchange_code = row[0],
+                    exchange_name = row[1],
+                    exchange_timezone = row[3],
+                    exchange_country = row[6]
+                )
+                new_exchange.save()
+                print('New exchange data saved to db')
+            
+f = FinnhubClient()
