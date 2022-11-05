3ac55f39fb7377dabdd4c1fcbd49b3491bcde2e9
jhtkoo0426 <jhtkoo@gmail.com>
2022-11-05 15:30:48 +0000

    Added models for supported exchanges and symbols

--- guiana/finnhub_integration/models.py
+++ guiana/finnhub_integration/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class FinnhubSupportedExchanges(models.Model):
+    exchange_code = models.CharField(max_length=4, blank=False, null=True, unique=True)
+    exchange_name = models.CharField(max_length=100, blank=True, null=True, unique=True)
+    exchange_timezone = models.CharField(max_length=100, blank=True, null=True, unique=False)
+    exchange_country = models.CharField(max_length=100, blank=False, null=True, unique=False)
+
+
+class FinnhubSupportedStockSymbols(models.Model):
+    symbol_exchange_code = models.ForeignKey(FinnhubSupportedExchanges, on_delete=models.CASCADE)
+    symbol_currency = models.CharField(max_length=100, blank=False, null=True, unique=False)
+    symbol_description = models.CharField(max_length=200, blank=True, null=True, unique=False)
+    symbol_type = models.CharField(max_length=100, blank=False, null=True, unique=False)
+
+    # Normally symbol_display_sym should be the same as symbol_name
+    symbol_display_sym = models.CharField(max_length=100, blank=True, null=True, unique=True)
+    symbol_name = models.CharField(max_length=100, blank=True, null=True, unique=True)
--- guiana/guiana/settings.py
+++ guiana/guiana/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'dashboard',
+    'finnhub_integration'
 ]
 
 MIDDLEWARE = [
